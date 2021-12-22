8b2e8d2e5ed958c4c75b767d509c9ce8ed7b7473
DarkJoker360 <simoespo159@gmail.com>
2021-12-22 11:58:39 +0100

    guacamole: Exclude updater
    * As I am doing personal builds this will
      prevent official updates to be tracked.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
